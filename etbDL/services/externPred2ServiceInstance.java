3a2993f64256949650d04769b492ec397cc51ec1
Tewodros Beyene <beyene@fortiss.org>
2019-05-23 11:44:45 +0200

    pushing uninit ETB

--- etbDL/services/externPred2ServiceInstance.java
+++ etbDL/services/externPred2ServiceInstance.java
@@ -4,24 +4,11 @@
 
 package etb.etbDL.services;
 import java.util.ArrayList;
-import etb.wrappers.*;
 
 public class externPred2ServiceInstance extends externPred2Service {
 	@Override
 	public genericWRP getGroundParams(String toolName, ArrayList<String> args, String mode) {
-		genericWRP genWRP = null;
-		if(toolName.equals("test2")){
-			genWRP = new test2WRP();
-		}
-		else if(toolName.equals("gccCompileLinux")){
-			genWRP = new gccCompileLinuxWRP();
-		}
-		else if(toolName.equals("testCC")){
-			genWRP = new testCCWRP();
-		}
-		else{
-			System.out.println("no external service found with name: " + toolName);
-		}
-		return genWRP;
+
+		return null;
 	}
 }
